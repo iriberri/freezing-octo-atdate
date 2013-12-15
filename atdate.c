25d4bc93a81a8c79d145c5ecadb4ff6727a91d0e
Carla Iriberri <iriberrig@gmail.com>
2013-12-15 12:21:00 +0100

    update

--- atdate.c
+++ atdate.c
@@ -1,8 +1,3 @@
-/*
-*Based on the stream socket client demo 
-*contained in Beej's Guide to Network Programming
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -13,330 +8,142 @@
 #include <arpa/inet.h>
 #include <time.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <signal.h>
-#include <errno.h>
 
-
-#define BACKLOG 10
-#define MAX_DATA_SIZE 100
-#define CLIENT_UDP_MODE 2
+#define SERVER_MODE 0
 #define CLIENT_TCP_MODE 1
+#define CLIENT_UDP_MODE 2
+#define DEBUG(d) if(debug_mode==1) {printf("[DEBUG %d] ", getpid()); printf(d); printf("\n");}
+#define ERROR(e) printf("[ERROR FATAL] "); printf(e); exit(-1);
 
 int sockfd;
 int acceptfd;
-int debug;
+char debug_mode;
+
+int help(int argc, char *argv[]) {
+    printf("\nEste programa puede ser usado de dos formas: como cliente o servidor\n");
+    printf("El uso es el siguiente: atdate [-h serverhost] [-p port] [-m cu | ct | s ] [-d]\n");
+    printf("\t[-h serverhost] indica a que servidor conectarse. Solo cuando esta en modo cliente.\n");
+    printf("\t[-p port] inidica a que puerto del servidor (especificado con -h) debe conectarse. Solo en modo cliente.\n");
+    printf("\t[-m modo] indica el modo de ejecucion\n");
+    printf("\t  ├── cu: cliente en UDP\n");
+    printf("\t  ├── ct: cliente en TCP\n");
+    printf("\t  └── s:  servidor\n");
+    return 0;
+}
 
 void signal_handler(int signal) {
-    printf("[CTRL+C] Closing socket.\n");
+    printf("[CTRL+C] Finalizando...\n");
     close(sockfd);
+    DEBUG("Cerrando socket..\n");
     close(acceptfd);
+    DEBUG("Socket cerrado.\n");
     exit(1);
 
 }
 
 void sigpipe_handler(int signal) {
     int pid = getpid();
-    printf("[pid=%d] Closed connection.\n", pid);
+    printf("[%d] Conexion cerrada.\n", pid);
     close(sockfd);
     exit(1);
 }
 
-void sigchld_handler(int s)
-{
-  while(waitpid(-1, NULL, WNOHANG)>0);
-}
+void new_client (char* serverhost_str, int type_of_connection, int port){
 
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
+    if (type_of_connection == CLIENT_UDP_MODE) {
+        printf("Iniciando modo cliente UDP a servidor %s\n", serverhost_str); 
+    } else {
+        printf("Iniciando modo cliente TCP a servidor %s\n", serverhost_str); 
     }
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-void client( char *host, int op_mode, char * port)
-{
-
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    char s[INET6_ADDRSTRLEN];
-    char date[MAX_DATA_SIZE];
-    size_t size=MAX_DATA_SIZE;
-    struct tm *time;
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    if (op_mode==CLIENT_TCP_MODE){
-
-        hints.ai_socktype = SOCK_STREAM;
-    }else if (op_mode == CLIENT_UDP_MODE){
-        hints.ai_socktype = SOCK_DGRAM;
+    struct hostent *he;
+    struct in_addr a;
+    he = gethostbyname (serverhost_str);
+    if (he) {
+        printf("Preguntando a %s ", he->h_name);
+        bcopy(*he->h_addr_list++, (char *) &a, sizeof(a));
+        printf("en %s\n", inet_ntoa(a));
     }
-    signal(SIGINT, signal_handler);
-    
-    if(debug)
-      printf("Looking for: %s %s \n", host,  port);
 
-    /*
-     * addrinfo -> DNS
-     */
-     if ((rv = getaddrinfo(host, port, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        exit(-1);
+    time_t time_int;
+    uint32_t recvt;
+    struct sockaddr_in client_address;
+    client_address.sin_family = AF_INET;
+    client_address.sin_addr.s_addr = inet_addr(inet_ntoa(a));
+    client_address.sin_port = htons(port);
+    if(type_of_connection == CLIENT_UDP_MODE) {
+        sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    } else {
+        sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     }
-    
-    
-    if(debug)
-      printf("Trying to create socket\n");
+    memset(&(client_address.sin_zero), '\0', 8);
 
-        // loop through all the results and connect to the first we can
-
-  for(p = servinfo; p != NULL; p = p->ai_next) {
-    if ((sockfd = socket(p->ai_family, p->ai_socktype,
-        p->ai_protocol)) == -1) {
-        perror("client: socket");
-    continue;
-}
-
-//f(op_mode==CLIENT_TCP_MODE){
-if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-    close(sockfd);
-    perror("client: connect");
-    continue;
-}
-//}
-break;
-}
-
-if (p == NULL) {
-    fprintf(stderr, "client: failed to connect\n");
-    exit(-1);
-}
-
-if(debug)
-  printf("I've got my socket on\n");
-
-inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-    s, sizeof(s));
-
-    freeaddrinfo(servinfo); // all done with this structure
-    
-
-    if(op_mode == CLIENT_TCP_MODE){
-        time_t t;       
-        int     n;           
-        int     outchars=MAX_DATA_SIZE;
-
-        if(debug)
-          printf("Waits for packet\n");
-
-        while(1)//reception loooooop
-        {
-            n = recv(sockfd, &t, outchars, 0);
-            if (n < 0)
-                fprintf(stderr, "socket read failed: %s\n", strerror(errno));
-            t=ntohl(t);
-
-            if(n==4)
-            {
-              if(debug)
-                printf("Received bytes: %d\n", n);
-
-            time=localtime(&t);
-            t= t - 2208988800u;
-            time=localtime(&t);
-            strftime ( date, size , "%d %b %Y %H:%M:%S %Z\n", time);
-            printf("%s\n", date);
-
+    int result = connect(sockfd, (struct sockaddr *)&client_address, sizeof(struct sockaddr));
+    if(type_of_connection == CLIENT_TCP_MODE && result == -1) {ERROR("Error al abrir conexión.")};
+    if(type_of_connection == CLIENT_UDP_MODE) {
+        DEBUG("Enviando datagrama vacio...");
+        send(sockfd, NULL, 0, 0); //Enviamos datagrama vacío
+        recv(sockfd, &recvt, 4, 0);
+        DEBUG("Recibida fecha..");
+        time_int = ntohl(recvt) - 2208988800; //Timestamp correspondiente al 0 para Linux (70 años)
+        printf("%s", ctime(&time_int));
+    }else{
+        while(recv(sockfd, &recvt, 4, 0) != 0) {
+            time_int = ntohl(recvt) - 2208988800; //Timestamp correspondiente al 0 para Linux (70 años)
+            printf("%s", ctime(&time_int));
         }
     }
 }
 
-if(op_mode == CLIENT_UDP_MODE){
+void server(int port) {
 
-    struct addrinfo * server=p; 
-    /* 
-     * addrinfo struct -> socket descriptor
-     * all we need: server struct and sockfd
-     */
+    struct sockaddr_in serveraddr;
 
-     time_t buf;  
-     int n;
-     int read_out;
+    unsigned int clientlen = 20;
+    time_t current_time;
 
-    // if((n= sendto(sockfd, &buf, 0 , 0, server->ai_addr, server->ai_addrlen))==-1)
-     if((n= send(sockfd, &buf, 0 , 0))==-1)
+    if(port == 37) port = 6002;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    //Usamos SO_REUSEADDR para que no tenga que esperar a que se libere el bind() del puerto
+    //en caso de detención brusca.
+    int optval = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
 
-     {
-        printf("Error when sending to the server\n");
-        exit(-1);
-    }
-
-    if(debug)
-      printf("Packet sent to %s \n", s);
-
-  read_out=4;
-
-  n = recvfrom(sockfd, &buf, read_out, 0, server->ai_addr, &server->ai_addrlen  );
-  if (n < 0)
-      fprintf(stderr, "socket read failed: %s\n", strerror(errno));
-
-  if(debug)
-      printf("%d bytes received. Date: %d\n", n, (int)buf);
-
-  buf= ntohl(buf);
-  time=localtime(&buf);
-  buf= buf - 2208988800u;
-  time=localtime(&buf);
-  strftime ( date, size , "%d %b %Y %H:%M:%S %Z\n", time);
-  printf("%s\n", date);
-
-  close(sockfd);
-}
-
-
-exit(0);
-}
-
-
-void server(char* port)
-{
-    printf("Esperando conexiones\n");
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr; // connector's address information
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes=1;
-    char s[INET6_ADDRSTRLEN];
-    int rv;
-
-    memset(&hints, 0, sizeof hints); //copy attr for future address struct
-    hints.ai_family = AF_UNSPEC; // any add family
-    hints.ai_socktype = SOCK_STREAM; //TCP
-    hints.ai_flags = AI_PASSIVE; // use my IP
-    
-    if(debug)
-      printf("Looking for %s \n", port);
-
-  if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) 
-  {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-    exit(-1);
-}
-
-if(debug)
-  printf("Trying to create socket\n");
-
-    //Look for all possible addresses and bind
-for(p = servinfo; p != NULL; p = p->ai_next) 
-{
-    if ((sockfd = socket(p->ai_family, p->ai_socktype,
-        p->ai_protocol)) == -1) {
-        perror("server: socket");
-    continue;
-}
-
-if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-    sizeof(int)) == -1) {
-    perror("setsockopt");
-exit(1);
-}
-
-if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-    close(sockfd);
-    perror("server: bind");
-    continue;
-}
-
-break;
-}
-
-if (p == NULL)  {
-    fprintf(stderr, "server: failed to bind\n");
-    exit(-1);   
-}
-
-freeaddrinfo(servinfo); 
-
-if (listen(sockfd, BACKLOG) == -1) {
-    perror("listen");
-    exit(1);
-}
-
-    sa.sa_handler = sigchld_handler; // reap all dead processes
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
-
-    while(1) { 
-        sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1) {
-            perror("accept");
-            continue;
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); //Escuchamos en cualquier dirección
+    serveraddr.sin_port = htons(port);
+    if (bind(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {ERROR("Error en bind")};
+    if (listen(sockfd, 10) < 0) {ERROR("Error en listen")};
+    printf("Servidor iniciado y escuchando en el puerto %d.\n", port);
+    uint32_t send_buffer;
+    while (1) {
+        struct sockaddr_in clientaddr;
+        acceptfd = accept(sockfd, (struct sockaddr *)&clientaddr, &clientlen);
+        if(fork() == 0) {
+            int pid = getpid();
+            int ret;
+            printf("[%d] [%s] Conexión abierta. Enviando informacion.\n", pid, inet_ntoa((struct in_addr)clientaddr.sin_addr));
+            do{
+                pid = getpid();
+                time(&current_time);
+                send_buffer = htonl(current_time + 2208988800);
+                sleep(1);
+                printf("[%d] [%s] Enviando fecha.\n", pid, inet_ntoa((struct in_addr)clientaddr.sin_addr));
+                ret = send(acceptfd, &send_buffer, sizeof(uint32_t), 0);
+                if(debug_mode==1) printf("[DEBUG] %d bytes enviados\n", ret);
+            }while( ret >0 );
+            printf("[%d] Conexion cerrada!\n", pid);
+            return;
         }
-        
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
-        if(debug)   
-            printf("New connection: %s, socket: %d\n", s, new_fd);
-        
-        printf("New connection  %s!\n", s);
-
-        if (!fork()) 
-        {
-            close(sockfd); 
-            time_t buff;
-            size_t size=sizeof(time_t);
-            if(debug)
-                printf("Time is: %d\n", (int)buff);
-
-            while(1)
-            {
-              /*
-               * Get time, transform to network format
-               * and send through socket new_fd
-               */
-               long int t= time(NULL);
-               time_t ti;
-               time(&ti);
-               buff=ti + 2208988800u;
-               buff=htonl(buff);
-               if(debug)
-                  printf("Send packet to %s, sockfd: %d\n", s, new_fd);
-              if (send(new_fd, &buff, size, 0) ==-1)
-              {
-                 fprintf(stderr, "Error: send %s\n", strerror(errno));
-                 exit(1);
-             }
-             else
-             {
-                 if(debug)
-                   printf("Packet has been sent!\n");
-           }
-
-           while(time(NULL)!=t+1 && time(0)<t+2);
-        //Waits for a second
-       }
-
-       close(new_fd);
-   }
-   close(new_fd);
+    }
 }
-}
-
-
 int main(int argc, char *argv[]) {
-
+    /* Parseamos los argumentos */
     if (argc == 1) {
-        printf("Help: %s [-h serverhost] [-p port] [-m cu | ct | s ] [-d]\n", argv[0]);
+        printf("ERROR - Parametros incorrectos.\n");
+        printf("Sintaxis: %s [-h serverhost] [-p port] [-m cu | ct | s ] [-d]\n", argv[0]);
         return -1;
     }
     signal(SIGPIPE, sigpipe_handler);
@@ -346,13 +153,17 @@ int main(int argc, char *argv[]) {
     char* serverhost_str;
     char* port_str;
     char* mode_str;
-    //char* port;
+    char debug_mode = 0;
+    int port = 37;
     for (param_c = 1; param_c < argc; param_c++) {
+        if (strcmp(argv[param_c], "--help") == 0) {
+            return help(argc, argv);
+        }
         if (strcmp("-d", argv[param_c]) == 0) {
-            debug = 1;
+            debug_mode = 1;
         } else {
             if (param_c + 2 > argc) {
-                printf("ERROR: invalid parameters.\n");
+                printf("ERROR: Parametros invalidos.\n");
                 return -1;
             }
             if (strcmp("-h", argv[param_c]) == 0) {
@@ -361,9 +172,9 @@ int main(int argc, char *argv[]) {
                 mode_str = strdup(argv[param_c + 1]);
             } else if (strcmp("-p", argv[param_c]) == 0) {
                 port_str = strdup(argv[param_c + 1]);
-                //port = atoi(port_str);
+                port = atoi(port_str);
             } else {
-                printf("ERROR: Parameter %s not recognized.\n", argv[param_c]);
+                printf("ERROR: Parametro %s no reconocido.\n", argv[param_c]);
                 return -1;
             }
 
@@ -371,19 +182,16 @@ int main(int argc, char *argv[]) {
         }
 
     }
-    port_str = strdup("37");
+
     if (strcmp(mode_str, "cu") == 0) {
-        client(serverhost_str, CLIENT_UDP_MODE, port_str);
+        new_client(serverhost_str, CLIENT_UDP_MODE, port);
     }else if (strcmp(mode_str, "ct") == 0) {
-        client(serverhost_str, CLIENT_TCP_MODE, port_str);
+        new_client(serverhost_str, CLIENT_TCP_MODE, port);
     }else if (strcmp(mode_str, "s") == 0) {
-        port_str = strdup("6283");
-        //port = prt;
-        server(port_str);
-    }else{
-        printf("Incorrect mode.\n");
-    }
-    return 0;
-
-
+     server(port);
+ }else{
+    printf("Modo incorrecto.\n");
+    help(argc, argv);
+}
+return 0;
 }
